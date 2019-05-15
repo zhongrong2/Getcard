5f41f40ccd664eff9a90a7a66bc430ef3fe3fbf2
zhongrong2 <1402458382@qq.com>
2019-05-15 09:32:55 +0800

    1

--- js/GetGarcard.js
+++ js/GetGarcard.js
@@ -1,16 +1,27 @@
-// 获取页面数据
-$.ajax({
-    url:"",
-    data:"",
-    method:"get",
-    dataType:"josn",
-    success:function (data) {
-        console.log(data);
-    },
-    error:function () {
-        console.log("服务器出错")
-    }
-})
+$(document).ready(function(){
+    var urlAndParam =location.href.split('#')[0];
+    var ID = $("#WeChatID").val();
+    $.ajax({
+        url: "http://lsh.longshihua.cn/WeChatManager/register/CheckweChatConfig",
+        data: {url:urlAndParam,ID: ID },
+        type: 'post',
+        dataType: "json",
+        success: function (data) {
+            var timestamp = data.timestamp;
+            var noncestr = data.nonceStr;
+            var signature = data.signature;
+            //通过config接口注入权限验证配置
+            wx.config({
+                debug: false,
+                appId: data.appId,
+                timestamp: timestamp,
+                nonceStr: noncestr,
+                signature: signature,
+                jsApiList: ['chooseImage','uploadImage']
+            });
+        }
+    });
+});
 //点击显示车牌号键盘
 $(".CarNum").click(function (e) {
     // console.log("1");
@@ -33,19 +44,6 @@ $("#box").click(function () {
 $("#select").click(function(e){
     closePro();
     var ul = $(this).children('ul');
-    // 获取下拉框数据
-    $.ajax({
-        url:"",
-        method:"get",
-        data:"",
-        dataType:"json",
-        success:function (data) {
-            console.log(data);
-        },
-        error:function () {
-            console.log("服务器出错")
-        }
-    })
     if(ul.is(":hidden")){
         ul.slideDown('400', function() {
             $(this).find("li").bind("click",function(){
@@ -100,23 +98,41 @@ function Get() {
         return false;
     }
     $.ajax({
-        url:"aaaaaa",
-        data:"",
-        method:"post",
+        url:"data.json",
+        // data:"",
+        method:"get",
         dataType:"json",
-        success:function () {
-
+        success:function (data) {
+            console.log(data);
+            if (data.status == 0){
+                $(".promptTxt").html(data.msg);
+                $("#prompt").show();
+            } else{
+                $(".promptTxt").html(data.msg);
+                $(".sure").hide();
+                $(".prompt-top img").attr("src","img/success.png")
+                $(".cancel").html("确定");
+                $(".cancel").css("width","100%");
+                $("#prompt").show();
+            }
         },
         error:function () {
-            console.log("服务器出错")
+            $(".ProTilet").html("服务器出错");
+            $("#probox").show();
+            hide();
         },
     })
     return true;
+}
+//点击弹窗确定
+function Sure() {
+    window.location.reload()//刷新当前页面.
 }
 //点击弹窗取消
 function Cancel() {
     $("#hint").hide();
     $("#prompt").hide();
+    wx.closeWindow();
 };
 // 弹窗隐藏
 function hide() {
